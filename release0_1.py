8e83cbeef9992cd22efac4f94a26098087e1fdb3
Mo <58116522+Metropass@users.noreply.github.com>
2020-09-23 17:18:07 -0400

    Finished URL_CHECKER.py
    Added my finished Python Script

--- release0_1.py
+++ release0_1.py
@@ -5,27 +5,48 @@ from colorama import Fore
 import sys
 
 
-@click.group()
-def cli():
-    pass
-    
-@cli.command('url')
-@click.argument('url')
-def meme2(url):
-    """this reads a url"""
+def basic_file_read(file):
     try:
-        link = urllib3.PoolManager()
-        res = link.request('HEAD', url)
-        if(res.status == 200):
-            print(Fore.GREEN + f"{url}  passes with {res.status}!")
-        elif (res.status == 403):
-            print(Fore.BLACK + f"{url} looks sus, it returned {res.status}")
-        else:
-            print(Fore.RED + f"{url} is dead, as it returned {res.status}")
+        file_data = open(file,'r',encoding="utf-8")
+        pattern = re.findall(r'https?:[a-zA-Z0-9_.+-/#~]+', file_data.read())
+        for l in pattern:
+            q = l.strip()
+            test_request(q)
     except OSError:
         print("The file cannot be opened! Make sure this file can be read and is legit.")
+
+def test_request(q):
+    try:
+        h = urllib3.PoolManager()
+        req = h.request('HEAD', q)
+        if(req.status == 200):
+            print(Fore.GREEN + f"{q}  passes with {req.status}!")
+        elif (req.status == 403):
+            print(Fore.WHITE + f"{q} looks sus, it returned {req.status}")
+        else:
+            print(Fore.RED + f"{q} is dead, as it returned {req.status}")
     except:
         print("Unknown Error: " + str(sys.exc_info()[0]))
+
+@click.group()
+def cli():
+    click.Context('--help')
+    pass
+
+@cli.command('file')
+@click.argument('file')
+def file_reader(file):
+    """this reads URL links from a file!"""
+    basic_file_read(file)
+
+@cli.command('url')
+@click.argument('url')
+def url_reader(url):
+    """this reads a URL that you pass as an argument!"""
+    test_request(str(url))
+
+
+
 
 if __name__ == '__main__':
     cli()
