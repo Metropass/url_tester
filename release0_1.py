a06f195056413ade513ab2c127f565cd79319ba4
Ruby <bautisruby643@gmail.com>
2020-10-01 21:50:34 -0400

    problem with the color output- check Click documentation with this

--- release0_1.py
+++ release0_1.py
@@ -8,24 +8,26 @@ import sys
 def basic_file_read(file, *args):
     s = args[0]
     try:
-        file_data = open(file,'r',encoding="utf-8")
+        file_data = open(file, 'r', encoding="utf-8")
         pattern = re.findall(r'https?:[a-zA-Z0-9_.+-/#~]+', file_data.read())
         for l in pattern:
             q = l.strip()
             test_request(q)
-            if(s):
+            if (s):
                 isHttp = re.match('(http)', q)
-                if(isHttp):
-                    q = re.sub('(http)','https', q)
+                if (isHttp):
+                    q = re.sub('(http)', 'https', q)
                     test_request(q)
     except OSError:
         print("The file cannot be opened! Make sure this file can be read and is legit.")
+
 
 def test_request(q):
     try:
         h = urllib3.PoolManager()
         req = h.request('HEAD', q)
-        if(req.status == 200):
+        if (req.status == 200):
+            # this is not working with my machine. Perhaps check Click documentation and see what is going on with this
             print(Fore.GREEN + f"{q}  passes with {req.status}!")
         elif (req.status == 403):
             print(Fore.WHITE + f"{q} looks sus, it returned {req.status}")
@@ -34,9 +36,11 @@ def test_request(q):
     except:
         print("Unknown Error: " + str(sys.exc_info()[0]))
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command('file')
 @click.argument('file')
@@ -45,11 +49,13 @@ def file_reader(file, s):
     """this reads URL links from a file!"""
     basic_file_read(file, s)
 
+
 @cli.command('url')
 @click.argument('url')
 def url_reader(url):
     """this reads a URL that you pass as an argument!"""
     test_request(str(url))
+
 
 @cli.command('version')
 def version_check():
