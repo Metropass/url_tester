609716cfde40737c5c6cf2bfc0fbc2a616cb6fa1
Mo <58116522+Metropass@users.noreply.github.com>
2020-10-09 17:43:20 -0400

    Added JSON notation

--- release0_1.py
+++ release0_1.py
@@ -1,92 +1,74 @@
 import click
 import urllib3
 import re
-# check this with Click documentation
 from colorama import Fore
 import sys
+import json
 
 
-def get_urls(data, *args):
-    s = args[0]
-    pattern = re.findall(r'https?:[a-zA-Z0-9_.+-/#~]+', data)
-    for l in pattern:
-        q = l.strip()
-        test_request(q)
-        if (s):
-            isHttp = re.match('(http)', q)
-            if (isHttp):
-                q = re.sub('(http)', 'https', q)
-                test_request(q)
-
-
-def website_read(q, s):
+def basic_file_read(file,json):
     try:
-        h = urllib3.PoolManager()
-        response = h.request('GET', q, timeout=5.0)
-    except:
-        print("This is an invalid link, please try again")
-    else:
-        try:
-            get_urls(response.data.decode('ISO-8859-1'), s)
-        except:
-            print("An error has occurred when retrieving the website")
-
-
-def basic_file_read(file, *args):
-    try:
-        file_data = open(file, 'r', encoding="utf-8")
-        get_urls(file_data.read(), *args)
+        file_data = open(file,'r',encoding="utf-8")
+        pattern = re.findall(r'https?:[a-zA-Z0-9_.+-/#~]+', file_data.read())
+        for l in pattern:
+            q = l.strip()
+            test_request(q,json)
     except OSError:
         print("The file cannot be opened! Make sure this file can be read and is legit.")
+        sys.exit(1)
 
-
-def test_request(q):
-    # accepts one url
-    # try adding more support for other html codes
+def test_request(q,json):
     try:
         h = urllib3.PoolManager()
         req = h.request('HEAD', q)
-        if (req.status == 200):
-            # this is not working with my machine. Perhaps check Click documentation and see what is going on with this
-            print(Fore.GREEN + f"{q}  passes with {req.status}!")
-        elif (req.status == 403):
-            print(Fore.WHITE + f"{q} looks sus, it returned {req.status}")
+        if not json:
+            if(req.status == 200):
+                print(Fore.GREEN + f"{q}  passes with {req.status}!")
+            elif (req.status == 403):
+                print(Fore.WHITE + f"{q} looks sus, it returned {req.status}")
+            else:
+                print(Fore.RED + f"{q} is dead, as it returned {req.status}")
         else:
-            print(Fore.RED + f"{q} is dead, as it returned {req.status}")
+            conv = {'url': q, 'status' : req.status}
+            print(conv)
     except:
         print("Unknown Error: " + str(sys.exc_info()[0]))
-
+        sys.exit(1)
 
 @click.group()
 def cli():
     pass
 
-
 @cli.command('file')
 @click.argument('file')
-@click.option('--s', is_flag=True, default=False, help='Optional flag to check if https can be used instead of http')
-def file_reader(file, s):
+@click.option('--json', is_flag=True)
+def file_reader(file,json):
     """this reads URL links from a file!"""
-    basic_file_read(file, s)
-
+    q = False
+    if json:
+        q = True
+    basic_file_read(file,q)
 
 @cli.command('url')
 @click.argument('url')
-@click.option('--l', is_flag=True, default=False, help='look through the given website recursively and check the webpage for '
-                                              'dead links')
-@click.option('--s', is_flag=True, default=False, help='Optional flag to check if https can be used instead of http')
-def url_reader(url, l, s):
+@click.option('--json', is_flag=True)
+
+
+def url_reader(url,json):
     """this reads a URL that you pass as an argument!"""
-    if l:
-        website_read(url, s)
-    else:
-        test_request(str(url))
+    q = False
+    if json:
+        q = True
+    test_request(str(url),q)
+
 
 
 @cli.command('version')
 def version_check():
     """Returns you the version number of this code"""
     print(Fore.BLUE + "Version 0.1")
+
+
 
 
 if __name__ == '__main__':
