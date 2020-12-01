827463ff6c729c1bf7d36a62ae72962232f6f7a1
Mo <58116522+Metropass@users.noreply.github.com>
2020-11-30 20:50:45 -0500

    Updated Release Test to latest version

--- release_test.py
+++ release_test.py
@@ -1,25 +1,23 @@
-import os
+import subprocess
 import pytest
 
 @pytest.fixture()
-def testing_url_list():
-    return ["https://youtube.ca", "https://gibhubfe.com/TASVideos", "http://www.danepstein.ca/category/open-source/feed"]
 
 def testing_url_good(url_arg):
     urls = testing_url_list()
-    a = os.system("python release0_1.py --url https://youtube.ca")
+    a = call("python release0_1.py --url https://youtube.ca")
     assert (a) == 200
 
 def test_url_404():
     urls = testing_url_list()
-    a = os.system("python release0_1.py --url https://gibhubfe.com/TASVideos")
+    a = call("python release0_1.py --url https://gibhubfe.com/TASVideos")
     assert (a) == 404
 
 def test_url_403():
     urls = testing_url_list()
-    a = os.system("python release0_1.py --url http://www.danepstein.ca/category/open-source/feed")
+    a = call("python release0_1.py --url http://www.danepstein.ca/category/open-source/feed")
     assert (a) == 403
 
 def error_check():
     url = "h://youtube.ca"
-    a = os.system("python release0_1.py --url {}".format(url))
+    a = call("python release0_1.py --url h://youtube.ca")
